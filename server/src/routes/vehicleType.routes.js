a149bfd16b094cc88ff6bfcc1e218d72f980bad1
LeTuanAnh <leanh250103@gmail.com>
2026-06-19 10:10:34 +0700

    docs(swagger): tách MasterData thành tag riêng từng resource (Vehicle Types/Floors/Zones/Parking Slots/Gates/Pricing Rules)

--- server/src/routes/vehicleType.routes.js
+++ server/src/routes/vehicleType.routes.js
@@ -7,29 +7,29 @@ import { vehicleTypeValidators, idParam } from '../validators/masterData.validat
 const router = Router();
 
 router.get('/',
-  /* #swagger.tags = ['MasterData']
+  /* #swagger.tags = ['Vehicle Types']
      #swagger.summary = 'Danh sách loại xe' */
   ...authenticated, vehicleTypeController.list);
 
 router.get('/:id',
-  /* #swagger.tags = ['MasterData']
+  /* #swagger.tags = ['Vehicle Types']
      #swagger.summary = 'Chi tiết loại xe' */
   ...authenticated, idParam, validate, vehicleTypeController.get);
 
 router.post('/',
-  /* #swagger.tags = ['MasterData']
+  /* #swagger.tags = ['Vehicle Types']
      #swagger.summary = 'Thêm loại xe (Manager)'
      #swagger.requestBody = { required: true, content: { 'application/json': { example: { typeName: 'Ô tô', typeCode: 'CAR' } } } } */
   ...managerWrite, vehicleTypeValidators.create, validate, vehicleTypeController.create);
 
 router.put('/:id',
-  /* #swagger.tags = ['MasterData']
+  /* #swagger.tags = ['Vehicle Types']
      #swagger.summary = 'Sửa loại xe (Manager)'
      #swagger.requestBody = { required: true, content: { 'application/json': { example: { typeName: 'Xe máy' } } } } */
   ...managerWrite, vehicleTypeValidators.update, validate, vehicleTypeController.update);
 
 router.delete('/:id',
-  /* #swagger.tags = ['MasterData']
+  /* #swagger.tags = ['Vehicle Types']
      #swagger.summary = 'Xóa loại xe (Manager)' */
   ...managerWrite, idParam, validate, vehicleTypeController.remove);
 
